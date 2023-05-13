a9689d3a7d1f2a3f80a7ec9aee8a955d97d2c55b
Edos Ddos <56231244+ekaramyan@users.noreply.github.com>
2023-05-13 14:11:01 +0400

    commit

--- utils.py
+++ utils.py
@@ -102,41 +102,10 @@ def add_to_my_cashbacks(cashbackActionId: int, cashbackItemId: int, cashbackUser
     print(cashbackItemId)
 
     params = {
-        'cashbackActionId': cashbackActionId,
+        # 'cashbackActionId': cashbackActionId,
         'cashbackItemId': cashbackItemId,
         'cashbackUserId': cashbackUserId,
-        'cashbackContractStatusId': cashbackContractStatusId,
-
-        'userPaid': True,
-        'userStartedAt': '2023-05-12T07:07:09.044Z',
-        'hasComplited': True,
-        'hasPaid': True,
-        'paidDocumentUrl': 'string',
-        'isCondition1': True,
-        'condition1Url': 'string',
-        'condition1Hours': 0,
-        'condition1FinishedAt': '2023-05-12T07:07:09.044Z',
-        'isCondition1CheckedByAdmin': True,
-        'isCondition2': True,
-        'condition2Url': 'string',
-        'condition2Hours': 0,
-        'condition2FinishedAt': '2023-05-12T07:07:09.044Z',
-        'isCondition2CheckedByAdmin': True,
-        'isCondition3': True,
-        'condition3Url': 'string',
-        'condition3Hours': 0,
-        'condition3FinishedAt': '2023-05-12T07:07:09.044Z',
-        'isCondition3CheckedByAdmin': True,
-        'isCondition4': True,
-        'condition4Url': 'string',
-        'condition4Hours': 0,
-        'condition4FinishedAt': '2023-05-12T07:07:09.044Z',
-        'isCondition4CheckedByAdmin': True,
-        'isCondition5': True,
-        'condition5Url': 'string',
-        'condition5Hours': 0,
-        'condition5FinishedAt': '2023-05-12T07:07:09.044Z',
-        'isCondition5CheckedByAdmin': True,
+        # 'cashbackContractStatusId': cashbackContractStatusId,
     }
 
     response = requests.post(URL + '/cashbacks/contracts', json=params, headers=HEADERS)
